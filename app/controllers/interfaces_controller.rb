74d384a961ada7697640364db7b559f849a99249
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2021-08-18 11:35:22 -0700

    add board show view
    Co-authored-by: dqveha <dqveha@users.noreply.github.com>
    Co-authored-by: professional-pigeon <professional-pigeon@users.noreply.github.com>

--- app/controllers/interfaces_controller.rb
+++ app/controllers/interfaces_controller.rb
@@ -2,12 +2,15 @@ class InterfacesController < ApplicationController
 
   def index
     response = HTTParty.get('http://localhost:3000/boards')
-    boards = JSON.parse(response.body).to_a
+    boards = JSON.parse(response.body)
     @boards = boards.map { |h| [h["name"], h["id"]] }
     render :index
   end
 
   def show
+    response = HTTParty.get("http://localhost:3000/boards/#{params[:id]}/posts")
+    posts = JSON.parse(response.body)
+    @posts = posts.map { |h| [h["title"], h["body"], h["username"], h["updated_at"]] }
     render :show
   end
 
