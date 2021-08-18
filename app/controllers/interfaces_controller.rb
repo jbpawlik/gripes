6fe8a1b829e1ac24fd10c0b208f0be3a1bcdba07
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2021-08-18 11:29:52 -0700

    add interface to board index
    Co-authored-by: dqveha <dqveha@users.noreply.github.com>
    Co-authored-by: professional-pigeon <professional-pigeon@users.noreply.github.com>

--- app/controllers/interfaces_controller.rb
+++ app/controllers/interfaces_controller.rb
@@ -2,19 +2,13 @@ class InterfacesController < ApplicationController
 
   def index
     response = HTTParty.get('http://localhost:3000/boards')
-    debugger
     boards = JSON.parse(response.body).to_a
-    @boards = boards.map { |h| [h["name"], h["id"] }
+    @boards = boards.map { |h| [h["name"], h["id"]] }
     render :index
   end
 
+  def show
+    render :show
+  end
+
 end
-
-@boards = boards.map { |h| {:name => h["name"], :id => h["id"]} }
-
-# number = @boards.length-1
-# number.times do
-#   ticker = 0
-#   @boards[ticker]["name"]
-#   ticker = ticker + 1
-# end
